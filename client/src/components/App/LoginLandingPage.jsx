f27d5908c3ce08347d479a6abab6ab9685fac997
Nathanvititoe <nathanvititoe@gmail.com>
2023-08-11 09:10:33 -0700

    removed unnecessary components

---
+++ client/src/components/App/LoginLandingPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const LoginLandingPage = () => {
+    return (
+        <>
+        <div id="pageContainer" className='bg-black'>
+      <div id="loginContainer">
+        <div id="loginInputBox">
+          <div id="loginTitle">
+              <img src="" alt="" id="Logo"/>
+              <h1 className='text-[red]'>Insight</h1>
+          </div>
+          <form action="POST">
+              <input type="text" />
+              <input type="text" />
+              <input type="submit" placeholder="Login" />
+          </form>
+        </div>
+      </div>
+    </div>
+    </>
+    )
+}
+export default LoginLandingPage;
