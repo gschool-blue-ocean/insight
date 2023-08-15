02f434c03a268a5f89cef77ff93cc60ee872a672
Nathanvititoe <nathanvititoe@gmail.com>
2023-08-15 14:31:41 -0700

    added themes for assignments component

--- client/src/components/App/NavMenu/Assignments.jsx
+++ client/src/components/App/NavMenu/Assignments.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LandingPageContext from "../LandingPage/LandingPageContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 const Assignments = () => {
+  const { isDarkMode } = useContext(LandingPageContext);
   //testdata
   const tableData = [
     {
@@ -23,6 +25,11 @@ const Assignments = () => {
     { title: "Assignment 3", submitted: true, comments: "you still suck" },
     { title: "Assignment 3", submitted: true, comments: "you still suck" },
     { title: "Assignment 3", submitted: true, comments: "you still suck" },
+    { title: "Assignment 3", submitted: true, comments: "you still suck" },
+    { title: "Assignment 3", submitted: true, comments: "you still suck" },
+    { title: "Assignment 3", submitted: true, comments: "you still suck" },
+    { title: "Assignment 3", submitted: true, comments: "you still suck" },
+    { title: "Assignment 3", submitted: true, comments: "you still suck" },
   ];
   const viewComment = () => {
     console.log("test");
@@ -35,15 +42,31 @@ const Assignments = () => {
       >
         <div
           id="assignmentHeader"
-          className="font-Sig text-[1.5rem] w-[95%] pt-[1.5rem] flex justify-center text-white"
+          className={
+            isDarkMode
+              ? "font-Sig text-[1.5rem] w-[95%] pt-[1.5rem] flex justify-center text-white"
+              : "font-Sig text-[1.5rem] w-[95%] pt-[1.5rem] flex justify-center text-black"
+          }
         >
-          <p className="font-bold text-[1.75rem] pb-[1.5rem] border-b-2 w-[95%] flex justify-center">
+          <p
+            className={
+              isDarkMode
+                ? "font-bold text-[1.75rem] pb-[1.5rem] border-b-2 w-[95%] flex justify-center"
+                : "font-bold text-[1.75rem] pb-[1.5rem] border-b-2 border-black w-[95%] flex justify-center"
+            }
+          >
             Assignments
           </p>
         </div>
-        <div className="w-[95%] mt-[.5rem] overflow-y-scroll max-h-[90%] min-h-[85%] scrollbar-thin scrollbar-track-ContentBGDM scrollbar-thumb-DGLogin">
+        <div
+          className={
+            isDarkMode
+              ? "w-[95%] mt-[.5rem] overflow-y-scroll max-h-[90%] min-h-[85%] scrollbar-thin scrollbar-track-ContentBGDM scrollbar-thumb-DOLogin"
+              : "w-[95%] mt-[.5rem] overflow-y-scroll max-h-[90%] min-h-[85%] scrollbar-thin scrollbar-track-BGboxLM scrollbar-thumb-LPLogin"
+          }
+        >
           <table className="w-full overflow-y-scroll max-h-[80%]">
-            <thead className="text-[#DCD3EB] text-[1.25rem]">
+            <thead className={isDarkMode ? "text-[#DCD3EB] text-[1.25rem]" : "text-DGrayLogin text-[1.25rem]"}>
               <tr>
                 <th className="py-[1rem]">Title</th>
                 <th>Submitted</th>
@@ -55,22 +78,29 @@ const Assignments = () => {
                 <tr
                   key={index}
                   className={
-                    index % 2 === 0
-                      ? "bg-DGLogin mt-[2rem] border-b-[1px]"
+                    isDarkMode
+                      ? index % 2 === 0
+                        ? "bg-DGLogin mt-[2rem] border-b-[1px]"
+                        : "mt-[2rem] border-b-[1px]"
+                      : index % 2 === 0
+                      ? "bg-LGLogin mt-[2rem] border-b-[1px]"
                       : "mt-[2rem] border-b-[1px]"
                   }
                 >
                   <td className="text-center py-[0.7rem]">{item.title}</td>
                   <td className="text-center">
                     <label htmlFor="checkbox1">
-                    <input
-                      type="checkbox"
-                      checked={item.submitted}
-                      className="w-5 h-5 appearance-none"
-                      id="checkbox1"
-                      readOnly
-                    />
-                    <FontAwesomeIcon className="text-[2rem] h-5 w-5 text-DOLogin" icon={faCheck}/>
+                      <input
+                        type="checkbox"
+                        checked={item.submitted}
+                        className="w-5 h-5 appearance-none"
+                        id="checkbox1"
+                        readOnly
+                      />
+                      <FontAwesomeIcon
+                        className={isDarkMode ? "text-[2rem] h-5 w-5 text-DOLogin" : "text-[2rem] h-5 w-5 text-LPLogin"}
+                        icon={faCheck}
+                      />
                     </label>
                   </td>
                   <td
