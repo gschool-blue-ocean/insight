b1abe88a8fb910f02e95815425f6e636e9f8481d
BAL17 <126348686+BAL17@users.noreply.github.com>
2023-08-16 12:11:26 -0500

    clustering implemented

--- api/src/server.js
+++ api/src/server.js
@@ -1,45 +1,94 @@
+//!devdependencies
 import express from "express";
 import cors from "cors";
 import pg from "pg";
+import cluster from "cluster";
+import dotenv from "dotenv";
+import os from "os";
 //!ROUTES imports
-import studentRoutes from './roles/studentRoutes.js'
+import studentRoutes from "./roles/studentRoutes.js";
 import assignmentRoutes from "./misc/assignments.js";
 import attendanceRoutes from "./misc/attendance.js";
 import gradeRoutes from "./misc/avg_grades.js";
-import instructorRoutes from './roles/instructorRoutes.js'
+import instructorRoutes from "./roles/instructorRoutes.js";
 import userRoutes from "./roles/userRoutes.js";
-
 import cohortRoutes from "./misc/cohort.js";
-
-import adminRoutes from './roles/adminRoutes.js'
-
-import dotenv from "dotenv";
-dotenv.config("api/.env");
-
-//DBSTRING CONNECTION
-const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+import adminRoutes from "./roles/adminRoutes.js";
+//env config pathing
+dotenv.config({ path: ".env" });
 
 const app = express();
+const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
-//MIDDLEWARES
-app.use(express.json());
-app.use(cors({ origin: "*" }));
-// app.use(express.static("dist"));
+//checkto see if the curr process is the primary process or a forked one
+if (cluster.isPrimary) { //if prim see total cpus aval on system 
+  const totalCPUs = os.cpus().length;
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Primary ${process.pid} is running`);
+//loop through all CPUS and fork worker proccess === to num of cpus aval. ea cpu has a worker
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
 
-//!Roles ROUTES
-app.use("/users", userRoutes);
+  cluster.on("exit", (worker) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
 
-//!MISC ROUTES
+  const logWorkerAndMemory = (req, res, next) => {
+    const workerId = cluster.isWorker ? cluster.worker.id : "Master";
+    const memoryUsage = process.memoryUsage().heapUsed;
+    console.log(`Worker ID: ${workerId}`);
+    console.log(`Memory Usage: ${memoryUsage} bytes`);
+    next();
+  };
+  //MIDDLEWARE
+  app.use(logWorkerAndMemory);
+  app.use(express.json());
+  app.use(cors({ origin: "*" }));
+  // ROUTES
+  app.use("/admin", adminRoutes);
+  app.use("/instructors", instructorRoutes);
+  app.use("/students", studentRoutes);
+  app.use("/users", userRoutes);
+  app.use("/assignments", assignmentRoutes);
+  app.use("/attendance", attendanceRoutes);
+  app.use("/grades", gradeRoutes);
+  app.use("/cohorts", cohortRoutes);
 
-//!ROUTES
-app.use('/admin', adminRoutes)
-app.use('/instructors', instructorRoutes)
-app.use('/students', studentRoutes)
-app.use("/users", userRoutes);
+  // ... Existing code for the clustering ...
+  app.get("/status", (req, res) => {
+    // Get worker id (if applicable)
+    const workerId = cluster.isWorker ? cluster.worker.id : "Master";
 
-app.use("/assignments", assignmentRoutes);
-app.use("/attendance", attendanceRoutes);
-app.use("/grades", gradeRoutes);
-app.use("/cohorts", cohortRoutes);
+    // Get memory usage
+    const memoryUsage = process.memoryUsage();
+
+    // Get system memory
+    const totalSystemMemory = os.totalmem();
+    const freeSystemMemory = os.freemem();
+
+    // Get CPU information
+    const cpus = os.cpus();
+
+    // Respond with the gathered information
+    res.status(200).json({
+      workerId,
+      memoryUsage,
+      totalSystemMemory,
+      freeSystemMemory,
+      cpus,
+    });
+  });
+
+const PORT = process.env.PORT;
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
 
 export { app, db };
