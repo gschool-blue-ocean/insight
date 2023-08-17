f25d94cff7febe7cd61077cf244d48f3506a7302
Nathanvititoe <nathanvititoe@gmail.com>
2023-08-17 10:29:16 -0700

    404 page

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,25 +1,19 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import LandingPageContext from "./App/LandingPage/LandingPageContext";
 import question from "/assets/question/questionDM.svg";
 const Error = () => {
-  const { isDarkMode } = useContext(LandingPageContext);
   return (
     <>
-      <div
-        className={
-          isDarkMode
-            ? "flex flex-col items-center pt-[10rem] h-screen text-white bg-DGLogin text-center"
-            : "flex flex-col items-center pt-[10rem] h-screen text-black bg-LGLogin text-center"
-        }
-      >
+      <div className="flex flex-col items-center pt-[10rem] h-screen text-white bg-DGLogin text-center">
         <img src={question} alt="question mark" />
         <h1 className="text-[6rem] border-DOLogin border-b-2">
           404 Page not found
         </h1>
         <div className="pt-[2rem]">
           <Link to="/">
-            <button className="p-3 border-black rounded-md hover:scale-[1.1] hover:shadow-focusDM-orange bg-BGboxDM">Back to Login</button>
+            <button className="p-3 border-black rounded-md hover:scale-[1.1] hover:shadow-focusDM-orange bg-BGboxDM">
+              Back to Login
+            </button>
           </Link>
         </div>
       </div>
