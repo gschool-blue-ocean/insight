440a2f56539876845320214f863d4bf6292b8dee
BAL17 <126348686+BAL17@users.noreply.github.com>
2023-08-17 11:57:10 -0500

    Co-authored-by: Isaac Summers <Sonapanic@users.noreply.github.com>

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./AuthFolder/ProtectedRoute.jsx";
 import LoginLandingPage from "./LandingPage/LoginLandingPage";
 import { LandingPageProvider } from "./LandingPage/LandingPageContext";
 import StudentsPage from "./StudentsPage/StudentsPage";
