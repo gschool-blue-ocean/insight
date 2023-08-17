cf79028efef69c56f20f3fea7094a7b90e416510
Nathanvititoe <nathanvititoe@gmail.com>
2023-08-17 08:29:26 -0700

    profile menu/ graph changes

--- client/src/components/App/StudentsPage/StudentsPage.jsx
+++ client/src/components/App/StudentsPage/StudentsPage.jsx
@@ -155,8 +155,8 @@ const StudentsPage = () => {
             id="workHub"
             className={
               isDarkMode
-                ? "bg-ContentBGDM w-[70%] h-full rounded-xl ml-[5rem] flex flex-col max-w-[1500px] justify-between"
-                : "bg-BGboxLM w-[70%] rounded-xl ml-[5rem] flex flex-col max-w-[1500px] justify-between"
+                ? "bg-ContentBGDM w-[70%] h-full rounded-xl ml-[5rem] flex flex-col max-w-[1500px]"
+                : "bg-BGboxLM w-[70%] h-full rounded-xl ml-[5rem] flex flex-col max-w-[1500px]"
             }
           >
             <Outlet />
