0e8e7f1aedbea0db585c9f028fc246d93600052f
Nathanvititoe <nathanvititoe@gmail.com>
2023-08-18 14:48:17 -0700

    adjusted navbar look

--- client/src/components/App/StudentsPage/StudentsPage.jsx
+++ client/src/components/App/StudentsPage/StudentsPage.jsx
@@ -104,50 +104,61 @@ const StudentsPage = () => {
         ) : null}
         <div
           id="contentContainer"
-          className="flex justify-center pt-[5%] h-[80%] font-Sig"
+          className="flex justify-start pt-[2%] h-[80%] font-Sig gap-[5rem]"
         >
           <div
             id="navMenu"
-            className="pl-[2rem] flex flex-col gap-[1.5rem] pt-[5rem]"
+            className=" flex flex-col h-full  pt-[2rem] w-[10rem]"
           >
-            <div id="assigmentsContainer" className="flex gap-[1rem]">
+            <div id="assigmentsContainer" className="flex cursor-pointer flex-col p-[2rem] gap-[1rem] hover:bg-ContentBGDM">
               {isDarkMode ? (
                 <img src={assignmentDM} alt="assignment icon" />
               ) : (
                 <img src={assignmentLM} alt="assignment icon" />
               )}
               <Link to="Assignments">
-                <h2 className="text-[1.5rem] cursor-pointer">Assignments</h2>
+                <h2 className="text-[1.25rem] text-center ">
+                  Assignments
+                </h2>
               </Link>
             </div>
-            <div id="messagesContainer" className="flex gap-[1rem]">
+            
+            <div id="messagesContainer" className="flex cursor-pointer flex-col gap-[1rem] p-[2rem] hover:bg-ContentBGDM">
               {isDarkMode ? (
                 <img src={messagesDM} alt="messages icon" />
               ) : (
                 <img src={messagesLM} alt="chat bubble icon" />
               )}
               <Link to="Messages">
-                <h2 className="text-[1.5rem] cursor-pointer">Messages</h2>
+                <h2 className="text-[1.25rem] text-center">
+                  Messages
+                </h2>
               </Link>
             </div>
-            <div id="calendarContainer" className="flex gap-[1rem]">
+            <div
+              id="calendarContainer"
+              className="flex cursor-pointer flex-col gap-[1rem] p-[2rem] hover:bg-ContentBGDM"
+            >
               {isDarkMode ? (
                 <img src={calendarDM} alt="calendar icon" />
               ) : (
                 <img src={calendarLM} alt="calendar icon" />
               )}
               <Link to="Calendar">
-                <h2 className="text-[1.5rem] cursor-pointer">Calendar</h2>
+                <h2 className="text-[1.25rem]  text-center">Calendar</h2>
               </Link>
             </div>
-            <div id="gradesContainer" className="flex gap-[1rem] pl-[2.5px]">
+            <div
+              id="gradesContainer"
+              className=" cursor-pointer flex flex-col gap-[1rem] p-[2rem] hover:bg-ContentBGDM"
+            >
               {isDarkMode ? (
                 <img src={gradesDM} alt="gradebook icon" />
               ) : (
                 <img src={gradesLM} alt="gradebook icon" />
               )}
               <Link to="Grades">
-                <h2 className="text-[1.5rem] cursor-pointer">Grades</h2>
+                <h2 className="text-[1.25rem]  text-center">Grades</h2>
               </Link>
             </div>
           </div>
