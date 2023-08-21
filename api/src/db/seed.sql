dad2c1800e581891dc625ce38de810781a18b521
Isaac Summers <monogator@gmail.com>
2023-08-21 09:29:46 -0700

    Removed avg_grades table from db, added join table for students and assignments. Also commented out test data not in use.

--- api/src/db/seed.sql
+++ api/src/db/seed.sql
@@ -3,24 +3,24 @@ VALUES
     ('MrHawkins', 'John', 'Hawkins', 'instructor'),
     ('BubbaHoundstooth', 'Temuera', 'Morrison', 'admin'),
     ('TrishJenkins', 'Trish', 'Jenkins', 'student'),
-    ('azure', 'Vivenna', 'Royal', 'student'),
-    ('spirit', 'Raoden', 'Sel', 'student'),
-    ('slipperypete', 'Art', 'Vandalay', 'student'),
-    ('sansabasongbird', 'Buster', 'Scruggs', 'student'),
-    ('kramerica', 'H.E.', 'Pennypacker', 'student'),
-    ('rogga', 'Alabaster', 'Tenring', 'student'),
-    ('crazydiamond', 'Josuke', 'Higashikata', 'student'),
-    ('sutarplachinum', 'Jotaro', 'Kujo', 'student'),
-    ('bootlip', 'Fudgepot', 'Cainforth-Cain', 'student'),
-    ('firstshot', 'Han', 'Solo', 'student'),
-    ('painbow', 'Jared', 'Carthalion', 'student'),
-    ('username', 'Warren', 'Atkinson', 'student'),
-    ('critfail', 'Matt', 'Mercer', 'student'),
-    ('slamacow', 'Finn', 'Mertins', 'student'),
-    ('giogio', 'Giorno', 'Giovana', 'student'),
-    ('president', 'Streetlamp', 'Le Moose', 'student'),
-    ('UGUD', 'Kevin', 'Pouya', 'student'),
-    ('syenite', 'Essun', 'Tirimo', 'instructor'),
+    -- ('azure', 'Vivenna', 'Royal', 'student'),
+    -- ('spirit', 'Raoden', 'Sel', 'student'),
+    -- ('slipperypete', 'Art', 'Vandalay', 'student'),
+    -- ('sansabasongbird', 'Buster', 'Scruggs', 'student'),
+    -- ('kramerica', 'H.E.', 'Pennypacker', 'student'),
+    -- ('rogga', 'Alabaster', 'Tenring', 'student'),
+    -- ('crazydiamond', 'Josuke', 'Higashikata', 'student'),
+    -- ('sutarplachinum', 'Jotaro', 'Kujo', 'student'),
+    -- ('bootlip', 'Fudgepot', 'Cainforth-Cain', 'student'),
+    -- ('firstshot', 'Han', 'Solo', 'student'),
+    -- ('painbow', 'Jared', 'Carthalion', 'student'),
+    -- ('username', 'Warren', 'Atkinson', 'student'),
+    -- ('critfail', 'Matt', 'Mercer', 'student'),
+    -- ('slamacow', 'Finn', 'Mertins', 'student'),
+    -- ('giogio', 'Giorno', 'Giovana', 'student'),
+    -- ('president', 'Streetlamp', 'Le Moose', 'student'),
+    -- ('UGUD', 'Kevin', 'Pouya', 'student'),
+    -- ('syenite', 'Essun', 'Tirimo', 'instructor'),
     ('MrsMarmalade', 'Jane', 'Marmalade', 'student');
 
 
@@ -38,29 +38,29 @@ VALUES
     (2, DATE '2024-02-15', DATE '2024-07-31', null); 
 
 
-INSERT INTO students (cohortId, userId, nps_rating, days_absent)
+INSERT INTO students (cohortId, userId, nps_rating, days_absent, avg_grade)
 VALUES
 
-    (1, 3, 3, null),
-    (1, 4, 3, null),  
-    (1, 5, 3, null), 
-    (1, 6, 3, 2), 
-    (1, 7, 3, null), 
-    (2, 8, 3, 5), 
-    (2, 9, 3, null), 
-    (2, 10, 3, null), 
-    (2, 11, 3, null), 
-    (2, 12, 3, null), 
-    (3, 13, 4, null),
-    (3, 14, 3, 1), 
-    (3, 15, 3, null), 
-    (3, 16, 3, null), 
-    (3, 17, 3, 2), 
-    (4, 18, 3, null), 
-    (4, 19, 3, 2), 
-    (4, 20, 3, null), 
-    (4, 21, 3, null), 
-    (4, 22, 3, 6);
+    (1, 3, 3, null, 90),
+    -- (1, 4, 3, null, 0, 88),  
+    -- (1, 5, 3, null, 0, 47), 
+    -- (1, 6, 3, 2, 0, 89), 
+    -- (1, 7, 3, null, 0, 78), 
+    -- (2, 8, 3, 5, 0, 78), 
+    -- (2, 9, 3, null, 0, 60), 
+    -- (2, 10, 3, null, 0, 90), 
+    -- (2, 11, 3, null, 0, 93), 
+    -- (2, 12, 3, null, 0, 93), 
+    -- (3, 13, 4, null, 0, 91),
+    -- (3, 14, 3, 1, 0, 88), 
+    -- (3, 15, 3, null, 0, 83), 
+    -- (3, 16, 3, null, 0, 70), 
+    -- (3, 17, 3, 2, 0, 99), 
+    -- (4, 18, 3, null, 0, 86), 
+    -- (4, 19, 3, 2, 0, 77), 
+    -- (4, 20, 3, null, 0, 92), 
+    -- (4, 21, 3, null, 0, 69), 
+    (2, 4, 3, 6, 78);
 
 
 INSERT INTO assignments (title, due_date, description, cohortId, instructor_comments)
@@ -69,36 +69,21 @@ VALUES
     ('Hackathon', DATE '2023-09-25', 'Your very own 1 day project', 2, null),
 
     ('Pixel Art Maker', DATE '2024-01-15', 'Create a pixel art maker', 1, null),  
-    ('Hackathon', DATE '2024-01-25', 'Your very own 1 day project', 1, null),  
+    ('Hackathon', DATE '2024-01-25', 'Your very own 1 day project', 1, null);
 
-    ('Pixel Art Maker', DATE '2023-10-15', 'Create a pixel art maker', 3, null),  
-    ('Hackathon', DATE '2023-10-25', 'Your very own 1 day project', 3, null),
+    -- ('Pixel Art Maker', DATE '2023-10-15', 'Create a pixel art maker', 3, null),  
+    -- ('Hackathon', DATE '2023-10-25', 'Your very own 1 day project', 3, null),
 
-    ('Pixel Art Maker', DATE '2024-03-01', 'Create a pixel art maker', 4, null),  
-    ('Hackathon', DATE '2024-03-15', 'Your very own 1 day project', 4, null);  
+    -- ('Pixel Art Maker', DATE '2024-03-01', 'Create a pixel art maker', 4, null),  
+    -- ('Hackathon', DATE '2024-03-15', 'Your very own 1 day project', 4, null);  
 
 
-INSERT INTO avg_grades (score, studentId)
-VALUES
-    (90, 1),  
-    (88, 2), 
-    (47, 3),
-    (89, 4),
-    (78, 5),
-    (78, 6),
-    (60, 7),
-    (90, 8),
-    (93, 9),
-    (88, 10),
-    (98, 11),
-    (93, 12),
-    (93, 13),
-    (91, 14),
-    (88, 15),
-    (83, 17),
-    (70, 18),
-    (99, 19),
-    (86, 20);
+INSERT INTO students_assignments (studentId, assignmentId, is_submitted, grade)
+VALUES 
+    (1, 1, false, null),
+    (1, 2, false, null),
+    (2, 1, false, null),
+    (2, 2, false, null);
 
 
 INSERT INTO admin (userId)
